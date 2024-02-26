bbea5edd1dfcf80f3e1b7a59f91cd57ba96ce03c
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-02-25 20:59:58 -0500

    Added different methods to the same URI

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.send('Received a GET HTTP method')
+});
+
+//post home page
+router.post('/', (req, res, next) => {
+  res.send('Received a POST HTTP method')
+});
+
+//post home page
+router.put('/', (req, res, next) => {
+  res.send('Received a PUT HTTP method')
+});
+
+//post home page
+router.delete('/', (req, res, next) => {
+  res.send('Received a DELETE HTTP method')
 });
 
 module.exports = router;
